1737ffa3ae909336c4332fd6fce53188d8d0ce41
ekimblejr <ekimblejr@gmail.com>
2023-12-22 19:01:53 -0500

    Added first Controller

--- Persistence/Migrations/20231222002236_InitialCreate.Designer.cs
+++ Persistence/Migrations/20231222002236_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231221220506_InitialCreate")]
+    [Migration("20231222002236_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
