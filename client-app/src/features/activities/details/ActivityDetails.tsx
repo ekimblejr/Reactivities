a22cceca56b38989d44c5eaa8bf7fa17935489e1
ekimblejr <ekimblejr@gmail.com>
2024-01-04 18:04:00 -0500

    Add MobX and refactor

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,17 +1,14 @@
 import { Button, ButtonGroup, Card, Image } from 'semantic-ui-react'
-import { Activity } from '../../../app/models/activity'
+import { useStore } from '../../../app/stores/store'
+import LoadingComponent from '../../../app/layout/LoadingComponent'
 
-interface Props {
-  activity: Activity
-  cancelSelectActivity: () => void
-  openForm: (id: string) => void
-}
+export default function ActivityDetails() {
 
-export default function ActivityDetails({
-  activity,
-  cancelSelectActivity,
-  openForm
-}: Props) {
+  const {activityStore} = useStore()
+  const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore
+
+  if (!activity) return <LoadingComponent />
+
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -26,7 +23,7 @@ export default function ActivityDetails({
         <ButtonGroup widths="2">
           <Button onClick={() => openForm(activity.id)} basic color="blue" content="Edit" />
           <Button
-            onClick={cancelSelectActivity}
+            onClick={cancelSelectedActivity}
             basic
             color="grey"
             content="Cancel"
