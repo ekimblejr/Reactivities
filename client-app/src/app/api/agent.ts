716f216c59742002220e0963b56040fcfc542f81
ekimblejr <ekimblejr@gmail.com>
2024-03-31 21:52:43 -0400

    Client side validation

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { Activity } from '../models/activity'
 import { toast } from 'react-toastify'
 import { router } from '../router/Routes'
 import { store } from '../stores/store'
+import { User, UserFormValues } from '../models/user'
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -10,12 +11,18 @@ const sleep = (delay: number) => {
   })
 }
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
-
 axios.defaults.baseURL = 'http://localhost:5005/api'
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data
+
+axios.interceptors.request.use(config => {
+  const token = store.commonStore.token
+  if (token && config.headers) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
 axios.interceptors.response.use(
-  async response => {
+  async (response) => {
     await sleep(1000)
     return response
   },
@@ -23,7 +30,10 @@ axios.interceptors.response.use(
     const { data, status, config } = error.response as AxiosResponse
     switch (status) {
       case 400:
-        if (config.method === 'get' && Object.prototype.hasOwnProperty.call(data.errors, 'id')) {
+        if (
+          config.method === 'get' &&
+          Object.prototype.hasOwnProperty.call(data.errors, 'id')
+        ) {
           router.navigate('/not-found')
         }
         if (data.errors) {
@@ -73,8 +83,16 @@ const Activities = {
   delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
+const Account = {
+  current: () => requests.get<User>('/account'),
+  login: (user: UserFormValues) => requests.post<User>('/account/login', user),
+  register: (user: UserFormValues) =>
+    requests.post<User>('/account/register', user)
+}
+
 const agent = {
-  Activities
+  Activities,
+  Account
 }
 
 export default agent
